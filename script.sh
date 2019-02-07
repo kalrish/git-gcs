763c8822bf772a452a8429af71ab2926358832b4
David Porcel <david.porcel@smaato.com>
2019-02-07 11:48:33 +0100

    Add export subcommand

--- script.sh
+++ script.sh
@@ -4,6 +4,51 @@ function command_archive {
 	git bundle create "${1:-${PWD##*/}.bundle}" --all
 }
 
+function command_export {
+	declare -A arguments
+	
+	declare -i count=2
+	
+	for argument in "$@"
+	do
+		if [[ ${argument} =~ ^\-\-(.*)=(.*)$ ]]
+		then
+			let ++count
+			
+			parameter="${BASH_REMATCH[1]}"
+			value="${BASH_REMATCH[2]}"
+			
+			if [[ -v arguments["${parameter}"] ]]
+			then
+				arguments["${parameter}"]="${value}"
+			fi
+		else
+			store="${argument}"
+			break
+		fi
+	done
+	
+	if [[ -v store ]]
+	then
+		for key in "${!arguments[@]}"
+		do
+			local -- "${key//-/_}=${arguments[${key}]}"
+		done
+		
+		declare -a key_args
+		for key in "${store}"/keys/*.asc
+		do
+			key_args+=(-f)
+			key_args+=("${key}")
+		done
+		
+		gpg --encrypt "${key_args[@]}" "${store}/store.yaml"
+	else
+		echo 'error: store name not specified'
+		return 1
+	fi
+}
+
 function command {
 	if [[ $# > 0 ]]
 	then
