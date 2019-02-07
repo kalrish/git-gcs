64350572c4a455dacd6f8caaddf7cd80d6f8ecac
David Porcel <david.porcel@smaato.com>
2019-02-07 13:34:52 +0100

    Add store subcommand

--- script.sh
+++ script.sh
@@ -136,6 +136,57 @@ function command_key {
 	fi
 }
 
+function command_store {
+	if [[ $# > 0 ]]
+	then
+		declare -A arguments
+		
+		declare -i count=2
+		
+		for argument in "$@"
+		do
+			if [[ ${argument} =~ ^\-\-(.*)=(.*)$ ]]
+			then
+				let ++count
+				
+				parameter="${BASH_REMATCH[1]}"
+				value="${BASH_REMATCH[2]}"
+				
+				if [[ -v arguments["${parameter}"] ]]
+				then
+					arguments["${parameter}"]="${value}"
+				fi
+			else
+				subcommand="${argument}"
+				break
+			fi
+		done
+		
+		if [[ -v subcommand ]]
+		then
+			funcmd="command_store_${subcommand}"
+			if [[ $(type -t -- "${funcmd}") = function ]]
+			then
+				for key in "${!arguments[@]}"
+				do
+					export -- "${key//-/_}=${arguments[${key}]}"
+				done
+				
+				"${funcmd}" "${@:${count}}"
+			else
+				echo "error: unknown git-gcs-store subcommand '${subcommand}'"
+				return 1
+			fi
+		else
+			echo 'error: missing git-gcs-store subcommand'
+			return 1
+		fi
+	else
+		echo 'error: missing git-gcs-store subcommand'
+		return 1
+	fi
+}
+
 function command {
 	if [[ $# > 0 ]]
 	then
